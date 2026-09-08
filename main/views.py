f2dcd8ec24fd275e69450e8f270a28a02887e62e
snweeza <nafeeza2948@gmail.com>
2026-09-08 21:17:46 +0700

    Tutorial 2: Implementasi MVT pada Django + Unit Test

---
+++ main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Nafeeza Arwatabina",
+        "npm": "2506604573",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Second year Undergraduate CS Student at Universitas Indonesia, " 
+            "who eager to learn and find a sliver of hope amidst the hardship."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Nafeeza Arwatabina",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
